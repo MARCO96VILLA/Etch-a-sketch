9479f90f5fa9bdf1d62a30d0c077acd53901f110
Marco Ferrari <marcofs1996@gmail.com>
2021-06-27 11:14:55 +0200

    first commit: grid, mouseover color change and reset button for new grid

---
+++ etch-a-sketch/script.js
@@ -0,0 +1,38 @@
+const container = document.getElementById("container");
+
+function makeRows(rows, cols) {
+    container.style.setProperty('--grid-rows', rows);
+    container.style.setProperty('--grid-cols', cols);
+    for (let c = 0; c < (rows * cols); c++) {
+      let cell = document.createElement("div");
+      container.appendChild(cell).className = "grid-item";
+    };
+    const gridItem = document.getElementsByClassName("grid-item");
+    for (let i = 0; i < gridItem.length; i++) {
+        gridItem[i].addEventListener("mouseover", function(){
+            gridItem[i].classList.add("grid-hover");
+    });
+}
+
+};
+
+makeRows(16, 16);
+
+
+let button = document.getElementById("reset");
+button.addEventListener("click", newGrid);
+
+function newGrid() {
+        while (container.firstChild) {
+        container.removeChild(container.lastChild);
+    }
+    let rowsInput = prompt("How many rows?");
+    let colInput = prompt("How many columns?");
+    let rowsInt = parseInt(rowsInput);
+    let colInt = parseInt(colInput);
+    if (rowsInt > 70 || colInt > 70) {
+        alert("The input was too high");
+    } else {
+        makeRows(rowsInt, colInt);
+    }
+}
